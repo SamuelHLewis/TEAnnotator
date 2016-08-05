18b1c73a676e842b66c2ae2eb5f30a5bb193f178
SamuelHLewis <samuel.h.lewis@gmail.com>
2016-08-05 15:39:22 +0100

    RepeatModeler module added

--- TEAnnotator.py
+++ TEAnnotator.py
@@ -1,12 +1,16 @@
 # Script to annotate the TE content of a genome using combined RepeatMasker & RepeatModeler
+# Tested on program versions: RepeatMasker open-4.0.6, RepeatModeler open-1.0.8, RepeatScout 1.0.5, TRF 4.0.4
 
 import os
 import sys
 import subprocess
 import random as random
+import shutil
 
 # input genome fasta file
 GenomeFile = 'dmel-X.fas'
+if GenomeFile.endswith('.fasta'):
+	os.rename(GenomeFile,GenomeFile.replace('.fasta','.fas'))
 # number of cores to use
 Cores = 8
 # species to use in RepeatMasker
@@ -23,6 +27,11 @@ def RepeatMaskerRunner(infile,cores,species):
 	os.remove(infile.replace('.fas','.fas.masked'))
 	os.remove(infile.replace('.fas','.fas.out'))
 	os.remove(infile.replace('.fas','.fas.tbl'))
+	# this removes the RepeatMasker temp dir
+	dirs = os.listdir(path='.')
+	for i in dirs:
+		if i.startswith('RM_'):
+			shutil.rmtree(i)
 	# in the gff, replace the TE RepeatMasker label ("similarity") with "exon" (so that gffread can read it)
 	cmd = 'sed s/similarity/exon/ <' + infile.replace('.fas','.fas.out.gff') + ' > ' + infile.replace('.fas','_TE.gff')
 	subprocess.call(cmd,shell=True)
@@ -32,6 +41,35 @@ def RepeatMaskerRunner(infile,cores,species):
 	cmd = 'grep -v \"rRNA\" ' + infile.replace('.fas','_TE.gff') + ' > ' + infile.replace('.fas','_TE_clean.gff')
 	subprocess.call(cmd,shell=True)
 	os.remove(infile.replace('.fas','_TE.gff'))
+
+# function to run RepeatModeler on genome
+def RepeatModelerRunner(infile,cores):
+	# # build database
+	# cmd = 'BuildDatabase -name ' + infile.strip('.fas') + ' -engine ncbi ' + infile
+	# subprocess.call(cmd,shell=True)
+	# # run RepeatModeler
+	# cmd = 'RepeatModeler -engine ncbi -pa ' + str(cores) + ' -database ' + infile.strip('.fas')
+	# subprocess.call(cmd,shell=True)
+	# # move RepeatModeler output "consensi.fa" (will be in randomly-named subdir) to current directory
+	# dirs = os.listdir(path='.')
+	# for i in dirs:
+	# 	if i.startswith('RM_'):
+	# 		RepModDir = i
+	# cmd = "mv ./" + RepModDir + '/' + 'consensi.fa.classified ' + infile.replace('.fas','_consensi.fa.classified')
+	# subprocess.call(cmd,shell=True)
+	# # remove database files & RepeatModeler temp dir	
+	# os.remove(infile.replace('.fas','.nhr'))
+	# os.remove(infile.replace('.fas','.nin'))
+	# os.remove(infile.replace('.fas','.nnd'))
+	# os.remove(infile.replace('.fas','.nni'))
+	# os.remove(infile.replace('.fas','.nog'))
+	# os.remove(infile.replace('.fas','.nsq'))
+	# os.remove(infile.replace('.fas','.translation'))
+	# os.remove('unaligned.fa')
+	# shutil.rmtree(RepModDir)
+	# run RepeatMasker on RepeatModeler output
+	cmd = ''
+	subprocess.call(cmd,shell=True)
 
 
 # function to combine multiple gffs, and output non-redundant gff and fasta files
@@ -59,6 +97,5 @@ def numberer(infile):
 		outfile.write(newline)
 		outfile.close()
 
-RepeatMaskerRunner(GenomeFile,Cores,Species)
-
-#test
+# RepeatMaskerRunner(GenomeFile,Cores,Species)
+RepeatModelerRunner(GenomeFile,Cores)
